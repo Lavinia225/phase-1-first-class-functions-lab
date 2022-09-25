a40c1d0b3c3bce4c21a0a2cff5ed05f10eb44955
Luna Seifert <thepuddingdeity@gmail.com>
2022-09-24 21:05:15 -0600

    commit tree b9bdbf8c189e14203f0441d4e4760b84c275bbf0

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+// Code your solution in this file
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(array.length - 2, array.length);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    return function(fare){
+        return fare * int;
+    };
+};
+
+const fareDoubler = fare => createFareMultiplier(2)(fare);
+const fareTripler = fare => createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers(drivers, fn){
+    return fn(drivers);
+}
